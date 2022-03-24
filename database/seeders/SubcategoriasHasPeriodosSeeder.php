85f282a822d53828e0832d0a019297a91763d2f4
jesusparra94 <jdparrau@gmail.com>
2022-03-24 19:22:15 -0300

    correccion de periodo preseleccionado

--- database/seeders/SubcategoriasHasPeriodosSeeder.php
+++ database/seeders/SubcategoriasHasPeriodosSeeder.php
@@ -14,105 +14,152 @@ class SubcategoriasHasPeriodosSeeder extends Seeder
      */
     public function run()
     {
+
         //Hosting SSH
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 1, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 1, 'periodo_id' => 3]);
-
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 1, 'periodo_id' => 4]);
 
         //Hosting amazon
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 2, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 2, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 2, 'periodo_id' => 4]);
 
         //Hosting Windows
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 3, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 3, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 3, 'periodo_id' => 4]);
 
         //Hosting Wordpress
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 4, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 4, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 4, 'periodo_id' => 4]);
 
         //Hosting Reseller
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 5, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 5, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 5, 'periodo_id' => 4]);
 
         //Tienda Online
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 6, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 6, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 6, 'periodo_id' => 3]);
+
+        //Certificado SSL
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 7, 'periodo_id' => 2]);
+
+        //Instalación Certificado SSL
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 8, 'periodo_id' => 5]);
 
         //Servidor VPS en Chile
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 9, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 9, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 9, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 9, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 9, 'periodo_id' => 4]);
 
         //Servidor VPS Windows
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 10, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 10, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 10, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 10, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 10, 'periodo_id' => 4]);
 
         //Servidor Linux Administrado
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 12, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 12, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 12, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 12, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 12, 'periodo_id' => 4]);
 
         //Servidor Amazon Linux
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 11, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 11, 'periodo_id' => 6]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 11, 'periodo_id' => 7]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 11, 'periodo_id' => 2]);
 
         //VPS Amazon windows
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 13, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 13, 'periodo_id' => 6]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 13, 'periodo_id' => 7]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 13, 'periodo_id' => 2]);
 
         //Administracion para VPS
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 14, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 14, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 14, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 14, 'periodo_id' => 3]);
 
         //Migración VPS
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 15, 'periodo_id' => 5]);
 
         //Servidor HP
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 16, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 16, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 16, 'periodo_id' => 3]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 16, 'periodo_id' => 4]);
 
         //Servidor DELL
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 17, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 17, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 17, 'periodo_id' => 3]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 17, 'periodo_id' => 4]);
 
         //Administtacion por evento
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 18, 'periodo_id' => 5]);
 
         //Streaming Radio
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 19, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 19, 'periodo_id' => 6]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 19, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 19, 'periodo_id' => 2]);
 
         //Google Workspace
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 20, 'periodo_id' => 1]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 20, 'periodo_id' => 2]);
+
+        //Migración Google Workspace
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 21, 'periodo_id' => 5]);
 
         //Google Ads
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 22, 'periodo_id' => 1]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 22, 'periodo_id' => 6]);
+
+        //Licencias Microsoft
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 23, 'periodo_id' => 5]);
 
         //Licencias Microsoft 365
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 24, 'periodo_id' => 1]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 24, 'periodo_id' => 2]);
+
+        //Soporte Microsoft 365
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 26, 'periodo_id' => 5]);
+
+        //Almacenamiento adicional google workspace
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 27, 'periodo_id' => 1]);
 
         //Housing
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 28, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 28, 'periodo_id' => 7]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 28, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 28, 'periodo_id' => 3]);
+
+        //Licencias Cpanel
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 29, 'periodo_id' => 1]);
+
+        //Licencias Imunify360
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 30, 'periodo_id' => 1]);
 
         //CloudBackup
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 32, 'periodo_id' => 1]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 32, 'periodo_id' => 7]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 32, 'periodo_id' => 2]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 32, 'periodo_id' => 3]);
 
         //Dominios
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 31, 'periodo_id' => 2]);
         SubcategoriasHasPeriodos::create(['subcategoria_id' => 31, 'periodo_id' => 3]);
+        SubcategoriasHasPeriodos::create(['subcategoria_id' => 31, 'periodo_id' => 4]);
 
     }
 }
